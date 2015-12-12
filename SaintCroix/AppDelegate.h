756a554b94186f17287ac35046b3ca3ef78b6bcf
Justin Haar <justin.haar22@gmail.com>
2015-12-12 09:26:12 -0500

    Initial Commit

---
+++ SaintCroix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaintCroix
+//
+//  Created by Justin Haar on 12/12/15.
+//  Copyright © 2015 Justin Haar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SaintCroix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SaintCroix
+//
+//  Created by Justin Haar on 12/12/15.
+//  Copyright © 2015 Justin Haar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
